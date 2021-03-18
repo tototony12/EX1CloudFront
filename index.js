a49dee6d29f395c45d066263c4cf5dae63859999
Antonio Ortiz <tototony@me.com>
2021-03-17 22:45:19 -0600

    index.js modificado

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ const api = express()
 
 api.use(express.static(__dirname + '/public'))
 
-api.listen(3001, (err) => {
+api.listen(3000, (err) => {
   if (err) {
     throw err
   }
   console.log('API corriendo')
 })
 
-app.use(bodyParser.urlencoded({ extended: false }))
+api.use(bodyParser.urlencoded({ extended: false }))
 
 api.post('/respuesta', (req,res) => {
-  const body = rew.body.body
+  const body = req.body.Body
   res.set('Content-Type', 'text/plain')
   res.send(`POST: ${body} recibido!`)
 })
