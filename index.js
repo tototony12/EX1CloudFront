1f2d033ebd227ac96b42a1f22c79e1734c1920db
Antonio Ortiz <tototony@me.com>
2021-03-17 22:37:14 -0600

    index.js modificado

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 'use strict'
 
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const api = express()
 
 api.use(express.static(__dirname + '/public'))
 
-api.listen(3001, () => {
+api.listen(3001, (err) => {
+  if (err) {
+    throw err
+  }
   console.log('API corriendo')
+})
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+api.post('/respuesta', (req,res) => {
+  const body = rew.body.body
+  res.set('Content-Type', 'text/plain')
+  res.send(`POST: ${body} recibido!`)
 })
